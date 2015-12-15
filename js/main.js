4d117a88bef1e9599c6c9c47f92bf808eded19de
knolegsb <seon2you@hotmail.com>
2015-12-15 01:18:30 -0800

    font awesome

--- js/main.js
+++ js/main.js
@@ -3,4 +3,24 @@ $(document).ready(function () {
     $('section#screenshots a').on('click', function () {
         $('div#modal img').attr('src', $(this).attr('data-image-url'));
     });
+
+    var nav = $('.navbar-fixed-top');
+    var distance = $('.navbar-fixed-top').offset();
+
+    //console.log(distance);
+
+    if (distance.top >= 300) {
+        nav.addClass('effect');
+    }
+
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+
+        //console.log(scroll);
+        if (scroll >= 300) {
+            nav.addClass('effect');
+        } else {
+            nav.removeClass('effect');
+        }
+    });
 });
